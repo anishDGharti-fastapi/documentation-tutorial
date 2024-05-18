92b08d65a198a60c19b7b71bb2fae4b09f53870a
Anish Gharti <anishDGharti@gmail.com>
2024-05-18 17:20:08 +0545

    Update pathParams.py

--- pathParams.py
+++ pathParams.py
@@ -4,3 +4,7 @@ from  main import app
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
